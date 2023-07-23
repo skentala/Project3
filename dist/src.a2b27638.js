5e9a2712b71bba3e474a8203ca0b56b045dd2a2e
skentala1 <135027565+skentala@users.noreply.github.com>
2023-07-23 11:06:55 +0000

    Update 4 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -945,7 +945,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42907" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40479" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
